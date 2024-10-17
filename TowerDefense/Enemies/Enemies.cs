1533123d8994803270887e4120e20beecdf0f1c9
Moritz <moritz.gaefeke@gmail.com>
2024-10-17 13:18:49 +0200

    enemies

--- TowerDefense/Enemies/Enemies.cs
+++ TowerDefense/Enemies/Enemies.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace TowerDefense.Enemies
 {
@@ -11,23 +8,49 @@ namespace TowerDefense.Enemies
         public int Speed { get; set; }
         public int Life { get; set; }
         public int Coins { get; set; }
+        public Point Position { get; set; }
 
         public Enemies(int speed, int life, int coins)
         {
             Speed = speed;
             Life = life;
             Coins = coins;
+
+            Movement();
         }
 
         public void Movement()
         {
+            for(int i = 1; i < GameHandler.CornerPoints; i++)
+            {
+                moveAnimation(Position, GameHandler.CornerPoints[i]);
+                Position = GameHandler.CornerPoints[i];
+            }
+        }
 
+        public void GetHit(int damage)
+        {
+            Life -= damage;
+
+            if(Life <= 0)
+            {
+                GetKilled();
+            }
+        }
+
+        public void MoveAnimation(Point PositionA, Point PosotionB)
+        {
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = PositionA,
+                To = PosotionB,
+                Duration = 2
+            };
         }
 
         public void GetKilled()
         {
-
+            GameHandler.AddCoins(Coins);
         }
-
     }
 }
--- TowerDefense/Enemies/Types/Goblin.cs
+++ TowerDefense/Enemies/Types/Goblin.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TowerDefense.Enemies.Types
+﻿namespace TowerDefense.Enemies.Types
 {
     internal class Giblin : Enemies
     {
         public Giblin() : base(speed: 10, life: 2, coins: 2)
         {
-
 
         }
     }
