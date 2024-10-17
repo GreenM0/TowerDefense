8d7c992ded68f36ac41c72612e7f1b6b7cfc48fb
Timon Abels <timon.abels03@gmail.com>
2024-10-17 14:54:56 +0200

    finished mainfunction of the game

--- TowerDefense/MainWindow.xaml.cs
+++ TowerDefense/MainWindow.xaml.cs
@@ -1,24 +1,20 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace TowerDefense
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            GameHandler gameHandler = new();
+            gameHandler.Width = MainGrid.ActualWidth;
+            gameHandler.Height = MainGrid.ActualHeight;
+            MainGrid.Children.Add(gameHandler);
         }
     }
 }
---
+++ TowerDefense/Maps/TestMap.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using System.Net;
+using System.Windows.Controls;
+
+namespace TowerDefense.Maps
+{
+    public partial class TestMap : UserControl
+    {
+        private Point[] _way = new Point[3];
+
+        private Point _startPoint { get; set; }
+        private Point _endPoint { get; set; }
+        private Point _corner1 { get; set; }
+
+        public TestMap()
+        {
+            InitializeComponent();
+            SetPoints();
+        }
+
+        private void SetPoints()
+        {
+            _startPoint = new Point
+            {
+                X = Convert.ToInt32(Line1.X1),
+                Y = Convert.ToInt32(Line1.Y1)
+            };
+
+            _corner1 = new Point
+            {
+                X = Convert.ToInt32(Line1.X2),
+                Y = Convert.ToInt32(Line1.Y2)
+            };
+
+            _endPoint = new Point
+            {
+                X = Convert.ToInt32(Line2.X2),
+                Y = Convert.ToInt32(Line2.Y2)
+            };
+
+            _way[0] = _startPoint;
+            _way[1] = _corner1;
+            _way[2] = _endPoint;
+        }
+
+        public Point[] Way()
+        {
+            return _way;
+        }
+    }
+}
