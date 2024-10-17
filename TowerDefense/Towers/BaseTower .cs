c422ba670931ab44d076f518ad452e0716d981cb
Lunis Wilken <lunisthebest@gmail.com>
2024-10-17 13:03:05 +0200

    Base claas for towers

---
+++ TowerDefense/Towers/BaseTower .cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Towers
+{
+    public abstract class BaseTower
+    {
+        public float AttackRange { get; private set; }
+        public float AttackSpeed { get; private set; }
+        public Point Position { get; set; }
+        public int AttackDamage { get; private set; }
+        public int Costs { get; private set; }
+        public float Size { get; private set; }
+
+        public BaseTower(float attackRange, int attackDamage, Point position, int costs, float size)
+        {
+            AttackDamage = attackDamage;
+            AttackRange = attackRange;
+            AttackSpeed = costs;
+            Position = position;
+            Costs = costs;
+            Size = size;
+        }
+
+        public void Attack()
+        {
+        }
+        public Enemies GetTarget(List<Enemy> enemies)
+        {
+        }
+
+        public bool IsInRange(Enemy enemy)
+        {
+            float distance = Point.Subtract(Position, enemy.Position).Length();
+            return distance <= AttackRange;
+        }
+        public List<EnemiesInRange> EnemiesInRange(Spatial grid, int cellSize)
+        {
+            var enemiesInRange = new List<Enemy>();
+
+            var cellsToCheck = GetCellsInRange(cellSize);
+
+            foreach (var cell in cellsToCheck)
+            {
+                var enemiesInCell = grid.GetEnemiesInCell(cell);
+                if (enemiesInCell != null)
+                {
+                    foreach (var enemy in enemiesInRange)
+                    {
+                        if (IsInRange(enemy))
+                        {
+                            enemiesInRange.Add(enemy);
+                        }
+                    }
+                }
+            }
+            return enemiesInRange;
+        }
+
+        public List<(int, int)> GetCellsInRange(int cellSize)
+        {
+            var cellsInRange = new List<(int, int)>();
+
+            (int centerX, int centerY) = ((int)(Position.X / cellSize), (int)(Position.Y / cellSize));
+
+            int rangeInCells = (int)Math.Ceiling(AttackRange / cellSize);
+
+            for (int x = -rangeInCells; x <= rangeInCells; x++)
+            {
+                for (int y = -rangeInCells; y <= rangeInCells; y++)
+                {
+                    cellsInRange.Add((centerX, centerY));
+                }
+            }
+                
+            return cellsInRange;
+        }
+
+    }   
+}
---
+++ TowerDefense/Towers/TowerTypes/TestTower1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TowerDefense.Towers.TowerTypes
+{
+    public class TestTower1 : BaseTower
+    {
+        public TestTower1(Point postion)
+            : base(attackRange: 10, attackDamage: 2, position: postion, costs: 10, size: 5)
+        {
+                
+        }
+    }
+}
